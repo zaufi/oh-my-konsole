8c35162ebd75e0ce6a0316abcc5bd68c35a01e55
Alex Turbov <i.zaufi@gmail.com>
2022-12-08 20:44:48 +0400

    feat: Add `pre-commit` hooks and fix `shellcheck` errors

--- install.sh
+++ install.sh
@@ -15,8 +15,9 @@ __EOF__
 ARGS=$(getopt -o "hsu" -l "help,system,user" -n "install.sh" -- "$@")
 
 # Check args
-if [ $? -ne 0 ]; then
-    echo "*** Error: Invalid agrument"
+# shellcheck disable=SC2181
+if [[ $? -ne 0 ]]; then
+    echo "*** Error: Invalid argument"
     exit 1
 fi
 
@@ -41,7 +42,7 @@ while true; do
             _dst_data_dir="${XDG_DATA_HOME:-${HOME}/.local/share}/konsole"
             _dst_sysconf_dir="${XDG_DATA_HOME:-${HOME}/.local/etc}"
             _dst_sample_config="${HOME}/.config/oh-my-konsole.conf"
-            echo -e '\n*** NOTE: To activate color changer, add `. ${_dst_sysconf_dir}/profile.d/oh-my-konsole.sh` to your `~/.bashrc`\n'
+            echo -e "\n*** NOTE: To activate color changer, add '. ${_dst_sysconf_dir}/profile.d/oh-my-konsole.sh' to your '~/.bashrc'\n"
             shift
             ;;
         --)
@@ -51,16 +52,16 @@ while true; do
     esac
 done
 
-if [ -z "${_dst_data_dir}" ]; then
+if [[ -z ${_dst_data_dir} ]]; then
     echo "*** Error: Can't detect a destination path"
     exit 1
 fi
 
 # TODO Check shopt
 
-ls profile/* scheme/* | while read f; do
-    echo install -m 0644 -D ${f} "${DESTDIR}${_dst_data_dir}/$(basename ${f})"
-    install -m 0644 -D ${f} "${DESTDIR}${_dst_data_dir}/$(basename ${f})"
+for f in profile/* scheme/*; do
+    echo install -m 0644 -D "${f}" "${DESTDIR}${_dst_data_dir}/$(basename "${f}")"
+    install -m 0644 -D "${f}" "${DESTDIR}${_dst_data_dir}/$(basename "${f}")"
 done
 
 echo install -m 0644 -D conf.d/oh-my-konsole "${DESTDIR}${_dst_sample_config}"
--- oh-my-konsole.sh
+++ oh-my-konsole.sh
@@ -10,50 +10,52 @@
 
 # Try get config if available
 # 0. the system-wide
+# shellcheck source=./conf.d/oh-my-konsole
 [[ -f /etc/conf.d/oh-my-konsole ]] && . /etc/conf.d/oh-my-konsole
 # 1. user overrides
-[[ -f ${XDG_CONFIG_HOME:-${HOME}/.config}/oh-my-konsole.conf ]] && . ${XDG_CONFIG_HOME:-${HOME}/.config}/oh-my-konsole.conf
+# shellcheck source=/dev/null
+[[ -f ${XDG_CONFIG_HOME:-${HOME}/.config}/oh-my-konsole.conf ]] && . "${XDG_CONFIG_HOME:-${HOME}/.config}"/oh-my-konsole.conf
 
 #
 # Change current color scheme of running `konsole` session
 #
 function _omk_set_konsole_color_scheme()
 {
-    local -r scheme="$1"
-    /usr/bin/konsoleprofile colorscheme=$scheme
+    local -r scheme="${1}"
+    /usr/bin/konsoleprofile colorscheme="${scheme}"
 }
 
 function docker()
 {
     local -r _change_color=$( \
-        [[ "$1" == run \
-        || "$1" == attach \
-        || ("$1" == container && ("$2" == run || "$2" == attach)) \
+        [[ "${1}" == run \
+        || "${1}" == attach \
+        || ("${1}" == container && ("${2}" == run || "${2}" == attach)) \
         ]] && echo yes || echo no \
       )
     if [[ ${_change_color} == yes ]]; then
-        _omk_set_konsole_color_scheme ${OMK_DOCKER_SCHEME:-OMKDockerShell}
+        _omk_set_konsole_color_scheme "${OMK_DOCKER_SCHEME:-OMKDockerShell}"
     fi
     /usr/bin/docker "$@"
     if [[ ${_change_color} == yes ]]; then
-        _omk_set_konsole_color_scheme ${OMK_DEFAULT_SCHEME:-OMKDefault}
+        _omk_set_konsole_color_scheme "${OMK_DEFAULT_SCHEME:-OMKDefault}"
     fi
 }
 
 function ssh()
 {
-    _omk_set_konsole_color_scheme ${OMK_SSH_SCHEME:-OMKSSHShell}
+    _omk_set_konsole_color_scheme "${OMK_SSH_SCHEME:-OMKSSHShell}"
     /usr/bin/ssh "$@"
-    _omk_set_konsole_color_scheme ${OMK_DEFAULT_SCHEME:-OMKDefault}
+    _omk_set_konsole_color_scheme "${OMK_DEFAULT_SCHEME:-OMKDefault}"
 }
 
 function vagrant()
 {
     local -r vagrant_bin=$(/bin/which vagrant)
     if [[ $1 == ssh ]]; then
-        _omk_set_konsole_color_scheme ${OMK_SSH_SCHEME:-OMKSSHShell}
+        _omk_set_konsole_color_scheme "${OMK_SSH_SCHEME:-OMKSSHShell}"
         ${vagrant_bin} "$@"
-        _omk_set_konsole_color_scheme ${OMK_DEFAULT_SCHEME:-OMKDefault}
+        _omk_set_konsole_color_scheme "${OMK_DEFAULT_SCHEME:-OMKDefault}"
     else
         ${vagrant_bin} "$@"
     fi
@@ -61,14 +63,15 @@ function vagrant()
 
 function su()
 {
-    _omk_set_konsole_color_scheme ${OMK_ROOT_SCHEME:-OMKRootShell}
+    _omk_set_konsole_color_scheme "${OMK_ROOT_SCHEME:-OMKRootShell}"
+    # shellcheck disable=SC2117
     /bin/su "$@"
-    _omk_set_konsole_color_scheme ${OMK_DEFAULT_SCHEME:-OMKDefault}
+    _omk_set_konsole_color_scheme "${OMK_DEFAULT_SCHEME:-OMKDefault}"
 }
 
 function sudo()
 {
-    _omk_set_konsole_color_scheme ${OMK_ROOT_SCHEME:-OMKRootShell}
+    _omk_set_konsole_color_scheme "${OMK_ROOT_SCHEME:-OMKRootShell}"
     /usr/bin/sudo "$@"
-    _omk_set_konsole_color_scheme ${OMK_DEFAULT_SCHEME:-OMKDefault}
+    _omk_set_konsole_color_scheme "${OMK_DEFAULT_SCHEME:-OMKDefault}"
 }
